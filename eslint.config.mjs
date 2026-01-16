4317b8be9ad7eaaca7fcadf6dbacff6a6bc3e228
chebanliubomir <qwqzx1485@gmail.com>
2026-01-16 19:58:11 +0200

    added new features to eslint for testing

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,29 +1,29 @@
+import path from 'path'
 import eslint from '@eslint/js';
 import {defineConfig} from 'eslint/config';
 import tseslint from 'typescript-eslint';
 
+const __dirname = path.dirname(__filename)
+
 export default defineConfig(
   eslint.configs.recommended,
-  tseslint.configs.recommended,
+  ...tseslint.configs.recommended,
   tseslint.configs.strict,
   tseslint.configs.stylistic,
   {
-
+    parser: '@typescript-eslint/parser',
+    files: ['*.ts', '*.tsx'],
+    parserOptions: {
+      project: [
+        './frontend/tsconfig.json',
+        './backend/tsconfig.json',
+        './tsconfig.json'
+      ],
+      tsconfigRootDir: __dirname,
+    },
     rules: {
       "@typescript-eslint/array-type": "error",
-      "@typescript-eslint/no-extraneous-class": "off"
-      // 'import/order': ['error', {
-      //   groups: [
-      //     'builtin',
-      //     'external',
-      //     'internal',
-      //     'parent',
-      //     'sibling',
-      //     'index',
-      //     'object',
-      //     'type'
-      //   ]
-      // }],
+      "@typescript-eslint/no-extraneous-class": "off",
     }
   }
 );
