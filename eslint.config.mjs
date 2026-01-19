0aa9648d817d723983c72e184609b0f3885bb5da
chebanliubomir <qwqzx1485@gmail.com>
2026-01-19 18:57:27 +0200

    remove all settings from to eslint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,28 +1,5 @@
-import path from 'path'
-import eslint from '@eslint/js';
 import {defineConfig} from 'eslint/config';
-import tseslint from 'typescript-eslint';
 
-const __dirname = path.dirname(__filename)
-
-export default defineConfig(
-  eslint.configs.recommended,
-  ...tseslint.configs.recommended,
-  tseslint.configs.strict,
-  tseslint.configs.stylistic,
-  {
-    // parser: '@typescript-eslint/parser',
-    // files: ['*.ts', '*.tsx'],
-    // parserOptions: {
-    //   project: [
-    //     './frontend/tsconfig.json',
-    //     './backend/tsconfig.json',
-    //     './tsconfig.json'
-    //   ],
-    // },
-    rules: {
-      "@typescript-eslint/array-type": "error",
-      "@typescript-eslint/no-extraneous-class": "off",
-    }
-  }
-);
+export default defineConfig([
+  {}
+]);
