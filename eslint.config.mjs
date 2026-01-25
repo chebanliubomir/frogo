5d9e1aec0b1d221ebf6b4f898eae3de8c7051f2b
chebanliubomir <qwqzx1485@gmail.com>
2026-01-25 20:26:08 +0200

    added was parts for frontend and backend to eslint file

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,7 +1,45 @@
+import globals from 'globals';
 import {defineConfig} from 'eslint/config';
+import tseslint from 'typescript-eslint';
 
 export default defineConfig([
   {
-    files: ['**/*.ts', '**/*.mts', '**/*.cts'],
-  }
+    ignores: [
+      "**/*.cjs",
+      "**/*.*.cjs",
+      "**/*.*.mjs",
+      "**/*.*.mjs",
+      "!**/eslint.*.mjs",
+    ],
+  },
+  {
+    overrides: [
+      //frontend
+      {
+        files: ["./frontend/**/*.{ts,tsx}"],
+        ignores: ["node_modules"],
+        rules: {},
+      },
+      //backend
+      {
+        files: ["./backend/**/*.{ts,mts,cts}"],
+        ignores: ["./backend/dist/**/*"],
+        plugins: {
+          "@typescript-eslint": tseslint.plugin,
+        },
+        languageOptions: {
+          parser: tseslint.parser,
+          parserOptions: {
+            project: ["./backend/tsconfig.json"],
+          },
+          globals: {
+            ...globals.browser,
+          },
+        },
+        rules: {
+          semi: ["error", "always"],
+        },
+      },
+    ],
+  },
 ]);
