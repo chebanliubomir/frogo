e164560e474625ce9341e52734482cfb8d2e0c1d
chebanliubomir <qwqzx1485@gmail.com>
2026-02-28 19:10:13 +0200

    recreated backend

--- backend/dist/src/main.js
+++
@@ -1,8 +0,0 @@
-import { AppModule } from './app.module.js';
-import { NestFactory } from '@nestjs/core';
-async function bootstrap() {
-    const app = await NestFactory.create(AppModule);
-    await app.listen(3000);
-}
-bootstrap();
-//# sourceMappingURL=main.js.map
--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -3,6 +3,6 @@ import { Module } from '@nestjs/common';
 @Module({
   imports: [],
   controllers: [],
+  providers: [],
 })
-
 export class AppModule {}
