ec4426811143fdf098c68bf05bbea124292221fe
chebanliubomir <qwqzx1485@gmail.com>
2026-03-11 00:22:45 +0200

    was changed default data for create user

--- backend/src/user/dto/create-user.dto.ts
+++ backend/src/user/dto/create-user.dto.ts
@@ -1,15 +1,15 @@
 import { ApiProperty } from "@nestjs/swagger"
 
 export class CreateUserDto {
-  @ApiProperty()
+  @ApiProperty({default: 'Dmitriy'})
   name: string
 
-  @ApiProperty()
+  @ApiProperty({default: 'Petrov'})
   surname: string
 
-  @ApiProperty()
+  @ApiProperty({default: 'qwerty@gmail.com'})
   email: string
 
-  @ApiProperty()
+  @ApiProperty({default: 'FcemD#orpfo#@#EmqQWEM'})
   password: string
 }
