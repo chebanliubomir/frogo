a8f09dae9563e4862a2d6daa3aef7be08d7d6185
chebanliubomir <qwqzx1485@gmail.com>
2026-03-13 20:09:30 +0200

    changed eslint config

--- backend/src/user/user.controller.ts
+++ backend/src/user/user.controller.ts
@@ -3,10 +3,10 @@ import { UserService } from './user.service.js';
 import { CreateUserDto } from './dto/create-user.dto.js'
 @Controller('user')
 export class UserController {
-  constructor(private readonly _userService: UserService) {}
+  constructor(private readonly userService: UserService) {}
   
   @Post('create')
   create(@Body() createUserDto: CreateUserDto) {
-    return this._userService.create(createUserDto)
+    return this.userService.create(createUserDto)
   }
 }
