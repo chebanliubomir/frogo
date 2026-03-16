94364d8015080cd7d3d75aad1521d163a5a48aaa
chebanliubomir <qwqzx1485@gmail.com>
2026-03-16 20:33:21 +0200

    was added method findOne for user

---
+++ backend/src/user/entities/user.entity.ts
@@ -0,0 +1,36 @@
+import { ApiProperty } from "@nestjs/swagger";
+import { Role, User } from "../../../prisma/generated/prisma/client.js";
+
+export class UserEntity implements User {
+
+  @ApiProperty()
+  id: number
+
+  @ApiProperty()
+  avatar: string
+
+  @ApiProperty()
+  name: string
+
+  @ApiProperty()
+  surname: string
+
+  @ApiProperty()
+  email: string
+  
+  @ApiProperty()
+  password: string
+
+  @ApiProperty({default: Role.USER})
+  rule: Role
+
+  @ApiProperty()
+  activatedLink: string
+
+  @ApiProperty()
+  updated_at: Date
+
+  @ApiProperty()
+  created_at: Date
+
+}
--- backend/src/user/user.controller.ts
+++ backend/src/user/user.controller.ts
@@ -1,12 +1,24 @@
-import { Controller, Post, Body } from '@nestjs/common';
+import { Controller, Post, Body, Param, Get, ParseIntPipe } from '@nestjs/common';
 import { UserService } from './user.service.js';
 import { CreateUserDto } from './dto/create-user.dto.js'
+import { ApiOkResponse, ApiTags } from '@nestjs/swagger';
+import { UserEntity } from './entities/user.entity.js';
 @Controller('user')
+@ApiTags('User')
 export class UserController {
+
   constructor(private readonly userService: UserService) {}
   
+  @ApiOkResponse({ type: UserEntity })
   @Post('create')
   create(@Body() createUserDto: CreateUserDto) {
     return this.userService.create(createUserDto)
   }
+  
+  @ApiOkResponse({ type: UserEntity })
+  @Get('one')
+  findOne(@Param('id', ParseIntPipe) id: number) {
+    return this.userService.findOne(id)
+  }
+
 }
