947fdc171294d06d645ca40d5ea1fdf59dadc0d9
chebanliubomir <qwqzx1485@gmail.com>
2026-03-22 23:55:44 +0200

    was deleted jwt folder and created jwt register to app module

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -3,7 +3,8 @@ import { ConfigModule } from '@nestjs/config';
 import { UserModule } from './user/user.module.js';
 import { PrismaModule } from './prisma/prisma.module.js';
 import { AuthenticationModule } from './authentication/authentication.module.js';
-import { JwtModule } from './jwt/jwt.module.js';
+import { JwtModule } from '@nestjs/jwt';
+import { env } from 'prisma/config';
 
 @Module({
   imports: [
@@ -11,10 +12,14 @@ import { JwtModule } from './jwt/jwt.module.js';
       envFilePath: [`.env.${process.env.NODE_ENV}`, '.env'],
       isGlobal: true
     }),
+    JwtModule.register({
+      global: true,
+      secret: env('JWT_SECRET_KEY'),
+      signOptions: { expiresIn: 60 }
+    }),
     AuthenticationModule,
     UserModule,
     PrismaModule,
-    JwtModule,
     ],
   controllers: [],
   providers: [],
--- backend/src/jwt/jwt.module.ts
+++
@@ -1,7 +0,0 @@
-import { Module } from '@nestjs/common';
-import { JwtService } from './jwt.service.js';
-
-@Module({
-  providers: [JwtService],
-})
-export class JwtModule {}
