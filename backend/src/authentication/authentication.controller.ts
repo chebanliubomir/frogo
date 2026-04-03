e6cd847397cfcc2a190c7f532c9fe542dad611e1
chebanliubomir <qwqzx1485@gmail.com>
2026-04-03 20:53:13 +0300

    added minor changed for authorization

--- backend/src/authentication/authentication.controller.ts
+++ backend/src/authentication/authentication.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Post, Body, Res, UseGuards, Request } from '@nestjs/common';
+import { Controller, Post, Body, Res, UseGuards, Req } from '@nestjs/common';
 import { AuthenticationService } from './authentication.service';
 import { RegistrationDto } from './dto/registration.dto';
 import { LoginDto } from './dto/login.dto';
@@ -26,25 +26,25 @@ export class AuthenticationController {
   @Post('login')
   async login(
     @Body() loginDto: LoginDto,
-    @Res() response: Response
+    @Res({ passthrough: true }) response: Response
   ) {
     const tokens = await this.authenticationService.login(loginDto);
 
-    response.cookie('refreshToken', tokens.refresh_token, {
-      httpOnly: true,
-      maxAge: 1000 * 60 * 60 * 24 * 7,
-    });
+    response.cookie('refreshToken', tokens.refresh_token);
 
-    return response.json({ access_token: tokens.access_token });
+    return response.json({ access_token: tokens.access_token })
   }
 
   @UseGuards(AuthenticationGuard)
   @Post('refresh')
-  async refresh(@Request() req) {
-    const user = req.user
-    console.log(user)
-    // const tokens = this.authenticationService.refresh();
-    // return tokens;
+  async refresh(
+    //problem with types for Request(express)
+    @Req() req
+  ) {
+    const user = await req.user
+    const cookie = await req.cookies
+    console.log(cookie)
+    return await this.authenticationService.refresh(user)
   }
 
 }
--- backend/src/authentication/guards/authentication.guard.ts
+++ backend/src/authentication/guards/authentication.guard.ts
@@ -1,20 +1,41 @@
 import { CanActivate, ExecutionContext, Injectable, UnauthorizedException } from '@nestjs/common';
 import { JwtService } from '@nestjs/jwt';
+import { User } from '@prisma/generated';
+import { Request } from 'express';
 
 @Injectable()
 export class AuthenticationGuard implements CanActivate {
 
-  constructor(private readonly jwt: JwtService) {}
+  constructor(private readonly jwt: JwtService) { }
 
   async canActivate(context: ExecutionContext) {
-    const request = context.switchToHttp().getRequest();
-    const token = request.headers.authorization.split(' ')[1]
-    console.log(token)
-    const payload = await this.jwt.verifyAsync(token)
+    try {
+      const request = context.switchToHttp().getRequest()
+      const token = this.expectTokenFromHeader(request)
+      if(!token) {
+        throw new UnauthorizedException()
+      }
 
-    request['user'] = payload
+      console.log(await request.cookie)
+      console.log(await request.cookies)
 
-    return true;
+      const payload = await this.checkValidToken(token);
+
+      request['user'] = payload
+
+    } catch {
+      throw new UnauthorizedException()
+    }
+    return true
+  }
+
+  private expectTokenFromHeader(request: Request): string | undefined {
+    const [type, token] = request.headers.authorization?.split(' ') ?? []
+    return type === "Bearer" ? token : undefined
+  }
+
+  private async checkValidToken(token): Promise<User> {
+    return await this.jwt.verifyAsync(token)
   }
 
 }
