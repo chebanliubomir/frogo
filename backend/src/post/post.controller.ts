f8b46d1bf8a71cfa32349854beff045520e99e53
chebanliubomir <qwqzx1485@gmail.com>
2026-05-16 20:04:51 +0300

    added functionality to post controller

--- backend/src/post/post.controller.ts
+++ backend/src/post/post.controller.ts
@@ -1,7 +1,30 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Delete, Get, Patch, Post } from '@nestjs/common';
 import { PostService } from './post.service';
+import { ApiOperation, ApiTags } from '@nestjs/swagger';
 
+@ApiTags('Post ')
 @Controller('post')
 export class PostController {
   constructor(private readonly postService: PostService) {}
+
+  @ApiOperation({summary: 'Create post'})
+  @Post('create')
+  async create() {}
+
+  @ApiOperation({summary: 'Update post'})
+  @Patch('update')
+  async update() {}
+
+  @ApiOperation({summary: 'Remove post'})
+  @Delete('remove')
+  async remove() {}
+
+  @ApiOperation({summary: 'Get only one post'})
+  @Get('get-one')
+  async getOne() {}
+
+  @ApiOperation({summary: 'Get all posts'})
+  @Get('get-all')
+  async getAll() {}
+
 }
