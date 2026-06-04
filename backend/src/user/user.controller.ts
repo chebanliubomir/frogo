f4ac65fa48cddae7cd766bc7922ba99c0210657d
chebanliubomir <qwqzx1485@gmail.com>
2026-06-04 21:00:40 +0300

    was added guard for checked role user

--- backend/src/user/user.controller.ts
+++ backend/src/user/user.controller.ts
@@ -1,6 +1,7 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, UseGuards } from '@nestjs/common';
 import { UserService } from './user.service';
 import { ApiOperation, ApiTags } from '@nestjs/swagger';
+import { AuthenticationGuard } from '@/authentication/guards/authentication.guard';
 @ApiTags('User')
 @Controller('user')
 export class UserController {
@@ -8,6 +9,7 @@ export class UserController {
   constructor(private readonly userService: UserService) {}
 
   @ApiOperation({ summary: 'Get list all users' })
+  @UseGuards(AuthenticationGuard)
   @Get('all')
   getAllUsers() {
     return this.userService.getAllUsers();
