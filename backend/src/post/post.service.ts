09ce40df055702a235973395f4a5b384f34181b4
chebanliubomir <qwqzx1485@gmail.com>
2026-06-29 00:32:53 +0300

    was minor changed

--- backend/src/post/post.service.ts
+++ backend/src/post/post.service.ts
@@ -1,87 +1,81 @@
-import { BadRequestException, Injectable } from '@nestjs/common'
+import { BadRequestException, Injectable } from '@nestjs/common';
 
-import { CreatePostDto } from './dto/create-post.dto'
-import { UpdatePostDto } from './dto/update-post.dto'
+import { CreatePostDto } from './dto/create-post.dto';
+import { UpdatePostDto } from './dto/update-post.dto';
 
-import { PrismaService } from '@/prisma/prisma.service'
+import { PrismaService } from '@/prisma/prisma.service';
+
+
+
 @Injectable()
 export class PostService {
+    constructor(private readonly prismaService: PrismaService) {}
 
-  constructor(
-    private readonly prismaService: PrismaService,
-  ) { }
+    async create(createPostDto: CreatePostDto, presentation: Express.Multer.File, userId: number) {
+        if (!createPostDto) throw new BadRequestException();
 
-  async create(createPostDto: CreatePostDto, presentation: Express.Multer.File, userId: number) {
+        const createPost = await this.prismaService.post.create({
+            data: {
+                title: createPostDto.title,
+                description: createPostDto.description,
+                userId: userId,
+            },
+        });
 
-    if(!createPostDto) throw new BadRequestException()
+        const createPresentation = await this.prismaService.presentation.create({
+            data: {
+                name: presentation.filename,
+                weight: presentation.size,
+                postId: createPost.id,
+            },
+        });
 
-    const createPost = await this.prismaService.post.create({
-      data: {
-        title: createPostDto.title,
-        description: createPostDto.description,
-        userId: userId
-      }
-    })
-
-    const createPresentation = await this.prismaService.presentation.create({
-      data: {
-        name: presentation.filename,
-        weight: presentation.size,
-        postId: createPost.id
-      }
-    })
-
-    
-
-    return {
-      ...createPost,
-      presentation: createPresentation
+        return {
+            ...createPost,
+            presentation: createPresentation,
+        };
     }
 
-  }
+    async getOne(id: number) {
+        const post = await this.prismaService.post.findUnique({ where: { id } });
+        if (!post) {
+            throw new BadRequestException();
+        }
 
-  async getOne(id: number) {
-    const post = await this.prismaService.post.findUnique({ where: { id } })
-    if (!post) {
-      throw new BadRequestException()
+        const presentation = await this.prismaService.presentation.findUnique({
+            where: { postId: post.id },
+        });
+
+        return {
+            ...post,
+            presentation,
+        };
     }
 
-    const presentation = await this.prismaService.presentation.findUnique({ where: { postId: post.id } })
+    async update(id: number, updatePostDto: UpdatePostDto) {
+        if (!updatePostDto) {
+            throw new BadRequestException();
+        }
 
-    return {
-      ...post,
-      presentation
+        const updatePost = await this.prismaService.post.update({
+            where: { id },
+            data: {
+                title: updatePostDto.title,
+                description: updatePostDto.description,
+            },
+        });
+
+        return updatePost;
     }
 
-  }
+    async remove(id: number) {
+        if (!id) {
+            throw new BadRequestException();
+        }
 
-  async update(id: number, updatePostDto: UpdatePostDto) {
+        await this.prismaService.presentation.delete({ where: { postId: id } });
+        await this.prismaService.post.delete({ where: { id } });
 
-    if (!updatePostDto) {
-      throw new BadRequestException()
+        return 'Post was remove.';
     }
-
-    const updatePost = await this.prismaService.post.update({
-      where: { id },
-      data: {
-        title: updatePostDto.title,
-        description: updatePostDto.description
-      }
-    })
-
-    return updatePost
-
-  }
-
-  async remove(id: number) {
-    if (!id) {
-      throw new BadRequestException()
-    }
-
-    await this.prismaService.presentation.delete({ where: { postId: id } })
-    await this.prismaService.post.delete({ where: { id } })
-
-
-    return 'Post was remove.'
-  }
 }
