de0cc606242da28d7ebd0fbdaf8d569b02a93c01
chebanliubomir <qwqzx1485@gmail.com>
2026-08-12 19:57:24 +0300

    minor changed

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -1,12 +1,3 @@
-import type { Metadata } from "next";
-import "./globals.css";
-
-
-export const metadata: Metadata = {
-  title: "Frogo",
-  description: "Frogo",
-};
-
 export default function RootLayout({ children }: LayoutProps<"/">) {
   return (
     <html lang="en">
