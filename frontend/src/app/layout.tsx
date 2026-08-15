8f915c2b5e69ecd0940aa4c56cfaa8aaa71cf0e1
chebanliubomir <qwqzx1485@gmail.com>
2026-08-15 20:00:07 +0300

    added shadcn ui

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -1,6 +1,12 @@
+import { Inter } from "next/font/google";
+import { cn } from "@/lib/utils";
+
+const inter = Inter({subsets:['latin'],variable:'--font-sans'});
+
+
 export default function RootLayout({ children }: LayoutProps<"/">) {
   return (
-    <html lang="en" suppressHydrationWarning>
+    <html lang="en" suppressHydrationWarning className={cn("font-sans", inter.variable)}>
       <body suppressHydrationWarning>{children}</body>
     </html>
   )
