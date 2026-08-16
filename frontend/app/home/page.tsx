dfb21e3d1d283769801dabf707b682848af20d3d
chebanliubomir <qwqzx1485@gmail.com>
2026-08-16 15:24:41 +0300

    added new component from shadcn ui library

--- frontend/app/home/page.tsx
+++ frontend/app/home/page.tsx
@@ -1,5 +1,7 @@
+import { Button } from "@/components/ui/button";
+
 export default function HomePage() {
   return (
-    <h1>Home page</h1>
+    <Button variant="outline">Home Page</Button>
   )
 }
