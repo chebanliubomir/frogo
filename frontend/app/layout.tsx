f48cae54e1ce634fae90f01ae9f3120ec478efb7
chebanliubomir <qwqzx1485@gmail.com>
2026-08-22 19:49:36 +0300

    update name derictory for wrapper component

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono, Inter } from "next/font/google";
 import "./globals.css";
 import { cn } from "@/lib/utils";
-import Wrapper from "@/components/Wrapper/wrapper";
+import Wrapper from "@/components/wrapper/wrapper";
 
 const inter = Inter({subsets:['latin'],variable:'--font-sans'});
 
