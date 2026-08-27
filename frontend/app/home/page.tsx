5a421ab5d7cb1c184022dda4bce83344b9a13622
chebanliubomir <qwqzx1485@gmail.com>
2026-08-27 20:53:37 +0300

    added component post

--- frontend/app/home/page.tsx
+++ frontend/app/home/page.tsx
@@ -1,9 +1,11 @@
 import Header from "@/components/Header/header";
+import Post from "@/components/Post/post";
 
 export default function HomePage() {
   return (
     <>
       <Header/>
+      <Post/>
     </>
   )
 }
---
+++ frontend/components/Post/post.tsx
@@ -0,0 +1,26 @@
+"use client"
+
+import Image from "next/image"
+import { AspectRatio } from "../ui/aspect-ratio"
+
+export default function Post() {
+  return (
+    <article>
+      <div className="intro">
+        <AspectRatio ratio={1 / 1}>
+        <Image src="" alt="Logo" className="w-full h-full rounded-md object-cover bg-amber-950" />
+      </AspectRatio>
+      </div>
+      <div className="title">
+        <h3>Title</h3>
+      </div>
+      <div className="description">
+        <p></p>
+      </div>
+      <div className="info">
+        <div className="downloads"><span>1</span></div>
+        <div className="purchases"><span>45</span></div>
+      </div>
+    </article>
+  )
+}
